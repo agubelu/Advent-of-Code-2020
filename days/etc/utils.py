ffc7b8d81ecd326b88c8b292a4277ef78c14a546
agubelu <agustinbd3@gmail.com>
2020-12-01 16:46:40 +0100

    Improved common input reader

--- days/etc/utils.py
+++ days/etc/utils.py
@@ -1,8 +1,8 @@
-def file_to_lines(filename, func=None):
+def file_to_lines(filename, func=None, spl="\n"):
     with open(filename, "r", encoding="utf-8") as f:
-        lines = [l.strip() for l in f.readlines()]
+        lines = [l.strip() for l in f.read().split(spl)]
     
-    lines = list(filter(bool, lines))
+    lines = list(filter(bool, lines)) # Remove empty lines
     if func:
         lines = list(map(func, lines))
     
