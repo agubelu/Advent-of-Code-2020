50d05276ae65731d937de434d6bc3eb4797f1f3a
agubelu <agustinbd3@gmail.com>
2020-12-06 15:42:34 +0100

    Further optimizations in Day 06 in Rust

--- rust/src/days/day06.rs
+++ rust/src/days/day06.rs
@@ -2,62 +2,54 @@ use std::boxed::Box;
 use std::error::Error;
 use std::io::{BufReader, BufRead};
 use std::fs;
-use std::collections::HashSet;
 use std::time::Instant;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-
 pub fn run() -> Result<(), Box<dyn Error>> {
-
     let time = Instant::now();
-
-    let mut shared_answers = 0;
-    let mut common_answers = 0;
 
     let f = BufReader::new(fs::File::open("../input/day06.txt").unwrap());
 
-    let mut combined_answers_set: HashSet<char> = HashSet::new();
-    let mut common_answers_set: HashSet<char> = HashSet::new();
+    let mut sol_part_1 = 0;
+    let mut sol_part_2 = 0;
 
-    let mut first = true;
+    let mut answers_and: u32 = 0;
+    let mut answers_or: u32 = 0;
 
     for line in f.lines() {
         let line = line.unwrap();
 
         if line == "" {
-            shared_answers += combined_answers_set.len();
-            common_answers += common_answers_set.len();
+            sol_part_1 += answers_or.count_ones();
+            sol_part_2 += answers_and.count_ones();
 
-            combined_answers_set = HashSet::new();
-            common_answers_set = HashSet::new();
-
-            first = true;
+            answers_or = 0;
+            answers_and = 0;
 
             continue;
         }
 
-        let mut person_answers_set = HashSet::new();
+        let mut answers: u32 = 0;
         for ch in line.chars() {
-            person_answers_set.insert(ch);
-            combined_answers_set.insert(ch);
+            answers |= 1 << (ch as u8);
         }
 
-        if first {
-            first = false;
-            common_answers_set = person_answers_set;
+        if answers_or == 0 {
+            answers_and = answers;
         } else {
-            common_answers_set = &common_answers_set & &person_answers_set;
+            answers_and &= answers;
         }
+
+        answers_or |= answers;
     }
 
-    shared_answers += combined_answers_set.len();
-    common_answers += common_answers_set.len();
+    sol_part_1 += answers_or.count_ones();
+    sol_part_2 += answers_and.count_ones();
 
-    let elapsed_ms = time.elapsed().as_nanos() as f64 / 1000000.0;
-    println!("Part 1: {}", shared_answers);
-    println!("Part 2: {}", common_answers);
+    let elapsed_ms = time.elapsed().as_nanos() as f64 / 1_000_000.0;
+    println!("Part 1: {}", sol_part_1);
+    println!("Part 2: {}", sol_part_2);
     println!("Elapsed: {:.3} ms", elapsed_ms);
     Ok(())
 }
-
