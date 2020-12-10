e276c54136a84bbf53e721582e4b11e65600e065
agubelu <agustinbd3@gmail.com>
2020-12-10 17:43:46 +0100

    Even better

--- days/day10.py
+++ days/day10.py
@@ -1,37 +1,25 @@
 from etc.utils import file_to_lines
-import networkx as nx
 import numpy as np
 
 lines = [0] + file_to_lines("input/day10.txt", func=int)
 n_lines = len(lines)
 lines.sort()
-target = lines[-1]
 
 # Part 1
 diffs = {1: 0, 2: 0, 3: 1}
-for i in range(1, n_lines):
-    diff = lines[i] - lines[i - 1]
-    diffs[diff] += 1
+for e1, e2 in zip(lines, lines[1:]):
+    diffs[e2 - e1] += 1
 
 print("Part 1:", diffs[1] * diffs[3])
 
 # Part 2
-graph = nx.DiGraph()
-for i in range(n_lines):
-    node = lines[i]
-    for other in lines[i + 1:i + 4]:
-        if other - node > 3: break
-        graph.add_edge(node, other)
+f = lambda i, j: 1 if j > i and lines[j] - lines[i] <= 3 else 0
+m = np.fromfunction(np.vectorize(f), (n_lines, n_lines), dtype=int)
+aux = np.identity(n_lines)
 
-adj_matrix = nx.to_numpy_matrix(graph)
-start_pow = len(nx.shortest_path(graph, 0, target)) - 1
-last_elem = adj_matrix.shape[0] - 1
+sol_part_2 = 0
+for _ in range(n_lines):
+    aux = aux @ m
+    sol_part_2 += aux[0, n_lines - 1]
 
-aux_mat = np.linalg.matrix_power(adj_matrix, start_pow)
-n_paths = aux_mat[0, last_elem]
-
-for i in range(start_pow, last_elem):
-    aux_mat = aux_mat @ adj_matrix
-    n_paths += aux_mat[0, last_elem]
-
-print("Part 2:", int(n_paths))
+print("Part 2:", int(sol_part_2))
