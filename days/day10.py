ef0f6dfc2be571dff5bec7acf9766bc6ca432882
agubelu <agustinbd3@gmail.com>
2020-12-10 17:09:11 +0100

    Optimization

--- days/day10.py
+++ days/day10.py
@@ -29,12 +29,14 @@ print("Part 1:", diffs[1] * diffs[3])
 
 # Part 2
 adj_matrix = nx.to_numpy_matrix(graph)
-adj_matrix_orig = adj_matrix.copy()
+start_pow = len(nx.shortest_path(graph, initial, target)) - 1
 last_elem = adj_matrix.shape[0] - 1
-n_paths = adj_matrix[0, last_elem]
 
-for i in range(last_elem):
-    adj_matrix = adj_matrix @ adj_matrix_orig
-    n_paths += adj_matrix[0, last_elem]
+aux_mat = np.linalg.matrix_power(adj_matrix, start_pow)
+n_paths = aux_mat[0, last_elem]
+
+for i in range(start_pow, last_elem):
+    aux_mat = aux_mat @ adj_matrix
+    n_paths += aux_mat[0, last_elem]
 
 print("Part 2:", int(n_paths))
