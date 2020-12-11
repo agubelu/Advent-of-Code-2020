5e7e849b879a36401fc11659ec1303ab891a7044
agubelu <agustinbd3@gmail.com>
2020-12-11 23:16:52 +0100

    Rewrite day 11

--- days/day11.py
+++ days/day11.py
@@ -1,74 +1,73 @@
 from etc.utils import file_to_lines
-import numpy as np
 
-initial = file_to_lines("input/day11.txt")
-shape = (len(initial), len(initial[0]))
+DIRS = [(-1, -1), (0, -1), (+1, -1),
+        (-1,  0),          (+1,  0),
+        (-1, +1), (0, +1), (+1, +1)]
 
-func_initial = lambda i, j: {".": 0, "L": 1, "#": 2}[initial[i][j]]
-initial_mat = np.fromfunction(np.vectorize(func_initial), shape, dtype=int)
-mat = initial_mat.copy()
+lines = file_to_lines("input/day11.txt")
+max_x = len(lines[0])
+max_y = len(lines)
 
-def func_next_1(i, j):
-    val = mat[i, j]
-    if val == 0: return val
-    adj = 0
+# Preamble
+def process_input(lines):
+    coords_map = {}
+    ls = []
+    inserted = 0
 
-    for n in range(max(0, i - 1), min(shape[0], i + 2)):
-        for m in range(max(0, j - 1), min(shape[1], j + 2)):
-            if (n != i or m != j) and mat[n,m] == 2:
-                adj += 1
+    for y, line in enumerate(lines):
+        for x, ch in enumerate(line):
+            if ch == "L":
+                ls.append(0)
+                coords_map[x, y] = inserted
+                inserted += 1
 
-    if val == 1 and adj == 0:
-        return 2
-    elif val == 2 and adj >= 4:
-        return 1
-    else:
-        return val
+    adj1 = {i: find_adj_1(coords, i, coords_map) for coords, i in coords_map.items()}
+    adj2 = {i: find_adj_2(coords, i, coords_map) for coords, i in coords_map.items()}
 
-f1 = np.vectorize(func_next_1)
-next_mat = np.fromfunction(np.vectorize(f1), shape, dtype=int)
-while np.any(next_mat != mat):
-    mat = next_mat
-    next_mat = np.fromfunction(np.vectorize(f1), shape, dtype=int)
+    return ls, adj1, adj2
 
-occupied = np.count_nonzero(mat == 2)
-print("Part 1:", occupied)
+def find_adj_1(coords, i, coords_map):
+    res = []
+    for dx, dy in DIRS:
+        x = coords[0] + dx
+        y = coords[1] + dy
 
-mat = initial_mat.copy()
-def func_next_2(i, j):
-    val = mat[i, j]
-    if val == 0: return val
-    adj = 0
-    diffs = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+        if 0 <= x < max_x and 0 <= y < max_y and (x, y) in coords_map:
+            res.append(coords_map[x, y])
+    return res
 
-    for dx, dy in diffs:
-        n, m = i+dx, j+dy
-        while 0 <= n < shape[0] and 0 <= m < shape[1]:
-            s = mat[n, m]
-            if s == 1:
+def find_adj_2(coords, i, coords_map):
+    res = []
+    for dx, dy in DIRS:
+        x = coords[0] + dx
+        y = coords[1] + dy
+
+        while 0 <= x < max_x and 0 <= y < max_y:
+            if (x, y) in coords_map:
+                res.append(coords_map[x, y])
                 break
-            elif s == 2:
-                adj += 1
-                break
-            n += dx
-            m += dy
-        
-    if val == 1 and adj == 0:
-        return 2
-    elif val == 2 and adj >= 5:
-        return 1
-    else:
-        return val
+            x += dx
+            y += dy
+    return res
 
+def solve(ls, adj_map, max_around):
+    def get_next(elem, i, cur_state):
+        count = sum(cur_state[j] for j in adj_map[i])
+        if elem == 0 and count == 0:
+            return 1
+        elif elem == 1 and count >= max_around:
+            return 0
+        else:
+            return elem
 
-f2 = np.vectorize(func_next_2)
-next_mat = np.fromfunction(np.vectorize(f2), shape, dtype=int)
-mat = next_mat
-next_mat = np.fromfunction(np.vectorize(f2), shape, dtype=int)
+    state = ls.copy()
+    next_state = [get_next(k, i, state) for i, k in enumerate(state)]
+    while state != next_state:
+        state = next_state
+        next_state = [get_next(k, i, state) for i, k in enumerate(state)]
+    return sum(next_state)
 
-while np.any(next_mat != mat):
-    mat = next_mat
-    next_mat = np.fromfunction(np.vectorize(f2), shape, dtype=int)
-
-occupied = np.count_nonzero(mat == 2)
-print("Part 2:", occupied)
+# Magic
+seats, adj1, adj2 = process_input(lines)
+print("Part 1:", solve(seats, adj1, 4))
+print("Part 2:", solve(seats, adj2, 5))
