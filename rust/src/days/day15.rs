b5b6693ec175ac52b32fcd4364c9e48e81d4f0a1
agubelu <agustinbd3@gmail.com>
2020-12-15 20:57:50 +0100

    u32 go brrr

--- rust/src/days/day15.rs
+++ rust/src/days/day15.rs
@@ -4,7 +4,7 @@ use std::time::Instant;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-const ITERS: usize = 30_000_000;
+const ITERS: u32 = 30_000_000;
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -12,9 +12,8 @@ pub fn run() {
     let time = Instant::now();
     let f = BufReader::new(File::open("../input/day15.txt").unwrap());
     let (mut mem, last_step) = get_initial_numbers(f);
-    let ini = last_step + 2;
 
-    let sol_part_1 = get_val(&mut mem, ini, 2020, 0);
+    let sol_part_1 = get_val(&mut mem, last_step + 2, 2020, 0);
     let sol_part_2 = get_val(&mut mem, 2021, ITERS, sol_part_1);
     
     let elapsed_ms = time.elapsed().as_nanos() as f64 / 1_000_000.0;
@@ -25,12 +24,12 @@ pub fn run() {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-fn get_val(mem: &mut Vec<usize>, initial_turn: usize, final_turn: usize, initial_val: usize) -> usize {
+fn get_val(mem: &mut Vec<u32>, initial_turn: u32, final_turn: u32, initial_val: u32) -> u32 {
     let mut last_num = initial_val;
 
     for i in initial_turn..=final_turn {
-        let last_seen = mem[last_num];
-        mem[last_num] = i-1;
+        let last_seen = mem[last_num as usize];
+        mem[last_num as usize] = i-1;
         last_num = match last_seen {
             0 => 0,
             x => i - x - 1,
@@ -40,19 +39,19 @@ fn get_val(mem: &mut Vec<usize>, initial_turn: usize, final_turn: usize, initial
     return last_num;
 }
 
-fn get_initial_numbers(mut f: BufReader<File>) -> (Vec<usize>, usize) {
+fn get_initial_numbers(mut f: BufReader<File>) -> (Vec<u32>, u32) {
     let mut s = String::new();
     let _ = f.read_line(&mut s);
     let split: Vec<&str> = s.split(',').collect();
 
-    let mut res = vec![0; ITERS];
+    let mut res = vec![0 as u32; ITERS as usize];
 
     split.iter()
          .enumerate()
          .for_each(|(i, spl)| {
-             let val = spl.parse::<usize>().unwrap();
-             res[val] = i + 1; 
+             let val = spl.parse::<u32>().unwrap();
+             res[val as usize] = i as u32 + 1; 
          });
 
-    return (res, split.len());
+    return (res, split.len() as u32);
 }
