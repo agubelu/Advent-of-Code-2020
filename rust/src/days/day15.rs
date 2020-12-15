84c0ff58b8dd54084f44b528dd90ae1c9132f628
agubelu <agustinbd3@gmail.com>
2020-12-15 18:30:03 +0100

    Upload day 15 input

--- rust/src/days/day15.rs
+++ rust/src/days/day15.rs
@@ -12,19 +12,20 @@ pub fn run() {
 
     let f = BufReader::new(File::open("../input/day15.txt").unwrap());
     let mut mem = get_initial_numbers(f);
-    let ini = mem.len() + 2;
+    let ini = mem.len() as u32 + 2;
 
     let sol_part_1 = get_val(&mut mem, ini, 2020, 0);
     let sol_part_2 = get_val(&mut mem, 2021, 30_000_000, sol_part_1);
-
+    
     let elapsed_ms = time.elapsed().as_nanos() as f64 / 1_000_000.0;
     println!("Part 1: {}", sol_part_1);
     println!("Part 2: {}", sol_part_2);
     println!("Elapsed: {:.3} ms", elapsed_ms);
 }
+
 ///////////////////////////////////////////////////////////////////////////////
 
-fn get_val(mem: &mut FxHashMap<usize, usize>, initial_turn: usize, final_turn: usize, initial_val: usize) -> usize {
+fn get_val(mem: &mut FxHashMap<u32, u32>, initial_turn: u32, final_turn: u32, initial_val: u32) -> u32 {
     let mut last_num = initial_val;
 
     for i in initial_turn..=final_turn {
@@ -44,15 +45,13 @@ fn get_val(mem: &mut FxHashMap<usize, usize>, initial_turn: usize, final_turn: u
     return last_num;
 }
 
-fn get_initial_numbers(mut f: BufReader<File>) -> FxHashMap<usize, usize> {
+fn get_initial_numbers(mut f: BufReader<File>) -> FxHashMap<u32, u32> {
     let mut s = String::new();
     let _ = f.read_line(&mut s);
     let split: Vec<&str> = s.split(',').collect();
 
-    let state = split.iter()
-                 .enumerate()
-                 .map(|(i, spl)| (spl.parse().unwrap(), i + 1))
-                 .collect();
-
-    return state;
+    return split.iter()
+                .enumerate()
+                .map(|(i, spl)| (spl.parse().unwrap(), i as u32 + 1))
+                .collect();
 }
