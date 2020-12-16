e5e1cb5ad70c58a9ec197fbd6d18bee885c93d07
agubelu <agustinbd3@gmail.com>
2020-12-16 20:24:31 +0100

    Style improvements

--- days/day15.py
+++ days/day15.py
@@ -4,17 +4,15 @@ ITERS = 30_000_000
 
 initial = file_to_lines("input/day15.txt", func=int, spl=",")
 mem = {n: i + 1 for i, n in enumerate(initial)}
-turn = len(initial) + 2
+last_turn = len(initial) + 2
 cur_num = 0
 
-while turn <= ITERS:
-    last_seen = mem.get(cur_num, 0)
+for turn in range(last_turn, ITERS + 1):
+    last_seen = mem.get(cur_num, None)
     mem[cur_num] = turn - 1
-    cur_num = 0 if last_seen == 0 else turn - last_seen - 1
+    cur_num = turn - last_seen - 1 if last_seen else 0
 
     if turn == 2020:
         print("Part 1:", cur_num)
-        
-    turn += 1
 
 print("Part 2:", cur_num)
